ac157d57f85b1b497cbf73a6e88665f52953a70c
TheGreyDiamond <s-einkauf@oest-rs.de>
2020-06-19 23:20:09 +0200

    Update OSTools.py

--- OSTools.py
+++ OSTools.py
@@ -57,7 +57,7 @@ def animate(state, direction, *args):
                 i += 1
         except Exception as exception:
             log.error("Could not animate menu: ", str(exception))
-            return(RobotError.Display.generalError)
+            return(RobotError.Display.Animation.generalError)
   
     else:
         i = 10
@@ -67,7 +67,7 @@ def animate(state, direction, *args):
                 i -= 1
         except Exception as exception:
             log.error("Could not animate menu: ", str(exception))
-            return(RobotError.Display.generalError)
+            return(RobotError.Display.Animation.generalError)
 
             
 
