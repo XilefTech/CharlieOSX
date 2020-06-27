b8a54b027caeb8a1fe4a597ff158b94986305466
XilefTech <XilefTech@users.noreply.github.com>
2020-06-27 23:09:57 +0200

    made settings changable

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 
-import time
-import math
-import _thread
-import ujson
+import time, math, json
 import OSTools as tools
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+from collections import OrderedDict
 
 import config
 
@@ -17,17 +15,30 @@ import config
 import robot
 
 import movements
-settings = 0
-def loadSettings():
-    with open('settings.json') as f:
-        return ujson.load(f)
 
-settings = {'Debug Driving': 2, 'Audio-Volume': 100, 'EFX-Volume': 100, 'Console-Log': True, 'Show Warnings': True, 'Show Errors': True}
+def loadSettings():
+    order = ['Debug Driving', 'Audio-Volume', 'EFX-Volume', 'Console-Log', 'Show Warnings', 'Show Errors']
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        sorted_settings = OrderedDict()
+        sorted_settings['options'] = OrderedDict()
+        for i in range(len(order)):
+            sorted_settings['options'][order[i]] = settings['options'][order[i]]
+        sorted_settings['values'] = settings['values']
+        return sorted_settings
 
 def storeSettings(data):
     with open('settings.json', 'w') as f:
-        ujson.dump(data, f)
+        f.write(json.dumps(data, sort_keys = False))
 
+settings = OrderedDict({'options': OrderedDict({'Debug Driving': 2, 'Audio-Volume': 100, 'EFX-Volume': 100, 'Console-Log': True, 'Show Warnings': True, 'Show Errors': True}),
+            'values': {
+                'min': {'Debug Driving': 0, 'Audio-Volume': 0, 'EFX-Volume': 0, 'Console-Log': False, 'Show Warnings': False, 'Show Errors': False},
+                'max': {'Debug Driving': 2, 'Audio-Volume': 100, 'EFX-Volume': 100, 'Console-Log': True, 'Show Warnings': True, 'Show Errors': True}
+            }})
+
+
+#storeSettings(settings) # temporary for storing changes in keys of the dictionary, created above, to the file
 
 charlie = EV3Brick()
 
@@ -37,6 +48,7 @@ def mainLoop():
     oldPos = 1
     loop, selected = True, False
     tools.drawMenu(int(menuState))
+    keys = list(settings['options'].keys())
     while loop:
         # navigation inbetween main pages
         if menuState == 0:
@@ -62,37 +74,54 @@ def mainLoop():
                     if position > 0:
                         position -= 1
                     elif position == 0:
-                        position = len(settings) - 1
+                        position = len(settings['options']) - 1
+                else:
+                    if settings['options'][keys[position]] > settings['values']['min'][keys[position]]:
+                        settings['options'][keys[position]] -= 1
+                    elif settings['options'][keys[position]] == settings['values']['min'][keys[position]]:
+                        settings['options'][keys[position]] = settings['values']['max'][keys[position]]
+                    tools.sound('media/click.wav')
+                    tools.drawSettings(position, settings, selected)
             if Button.DOWN in charlie.buttons.pressed():
                 if not selected:
-                    if position < len(settings) - 1:
+                    if position < len(settings['options']) - 1:
                         position += 1
-                    if position == len(settings) - 1:
+                    elif position == len(settings['options']) - 1:
                         position = 0
+                else:
+                    if settings['options'][keys[position]] < settings['values']['max'][keys[position]]:
+                        settings['options'][keys[position]] += 1
+                    elif settings['options'][keys[position]] == settings['values']['max'][keys[position]]:
+                        settings['options'][keys[position]] = settings['values']['min'][keys[position]]
+                    tools.sound('media/click.wav')
+                    tools.drawSettings(position, settings, selected)
+                        
 
 
             if Button.CENTER in charlie.buttons.pressed():
+                if selected:
+                    storeSettings(settings)
                 selected = not selected
                 oldPos += 1
-
 
             if position != oldPos:
                 tools.sound('media/click.wav')
                 time.sleep(0.08)
                 tools.drawSettings(position, settings, selected)
                 oldPos = position
-                time.sleep(0.17)
+                time.sleep(0.15)
 
         # selection of pages
-        if Button.RIGHT in charlie.buttons.pressed() and menuState > 0 and menuState <= 50:
+        if Button.RIGHT in charlie.buttons.pressed() and menuState > 0 and menuState <= 5 and not selected:
             menuState = menuState * 10
+            position, oldPos = 0, 1
             oldMenuState = menuState
             tools.sound('media/confirm.wav')
             time.sleep(0.08)
             tools.animate(menuState, True)
             time.sleep(0.4)
 
-        if Button.LEFT in charlie.buttons.pressed() and menuState >= 10:
+        if Button.LEFT in charlie.buttons.pressed() and menuState >= 10 and not selected:
             tools.sound('media/confirm.wav')
             time.sleep(0.08)
             tools.animate(menuState, False)
