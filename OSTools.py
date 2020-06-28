87900f7d2f3860142eb433d5be3b47599832a3cc
XilefTech <XilefTech@users.noreply.github.com>
2020-06-29 00:30:49 +0200

    temporarily removed changes from last commit due to many issues

--- OSTools.py
+++ OSTools.py
@@ -4,7 +4,6 @@ from pybricks.parameters import Align, Color, Button
 from pybricks.media.ev3dev import Image, ImageFile, Font, SoundFile
 from robotError import *
 import time, _thread
-import __main__
 
 
 charlie = EV3Brick()
@@ -136,7 +135,7 @@ class log:
 
     def error(msg, exception, *args):
         global logMsg
-        if __main__.settings['options']['Console-Log']:
+        if True:
             ts = time.localtime(time.time())
             print('[%d.%d.%d %d:%d:%d] [Error]' % (ts[2], ts[1], ts[0], ts[3], ts[4], ts[5]), msg, exception, *args)
         
@@ -169,7 +168,7 @@ class log:
     def warn(msg, *args):
         global logMsg
 
-        if __main__.settings['options']['Console-Log']:
+        if True:
             ts = time.localtime(time.time())
             print('[%d.%d.%d %d:%d:%d] [Warning]' % (ts[2], ts[1], ts[0], ts[3], ts[4], ts[5]), msg)
         
@@ -201,7 +200,7 @@ class log:
     
     def info(msg, *args):
         ''' Makes a log output, without showing anything on the EV3 screen'''
-        if __main__.settings['options']['Console-Log']:
+        if True:
             ts = time.localtime(time.time())
             print('[%d.%d.%d %d:%d:%d] [Info]' % (ts[2], ts[1], ts[0], ts[3], ts[4], ts[5]), msg)
     
