dacc258a30ed2ffbc582558c966a21ff58a9e7a3
XilefTech <XilefTech@users.noreply.github.com>
2020-07-29 19:36:24 +0200

    ordered stuff from main.py to ui and charlieosx

--- main.py
+++ main.py
@@ -9,153 +9,9 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from collections import OrderedDict
 
-import config
+import config, ui
 
-# initialize the Sensors and Motors
-import robot
-
-import movements
-
-def loadSettings():
-    order = ['Debug Driving', 'Audio-Volume', 'EFX-Volume', 'Console-Log', 'Show Warnings', 'Show Errors']
-    with open('settings.json', 'r') as f:
-        settings = json.load(f)
-        sorted_settings = OrderedDict()
-        sorted_settings['options'] = OrderedDict()
-        for i in range(len(order)):
-            sorted_settings['options'][order[i]] = settings['options'][order[i]]
-        sorted_settings['values'] = settings['values']
-        sorted_settings['types'] = settings['types']
-        return sorted_settings
-
-def storeSettings(data):
-    with open('settings.json', 'w') as f:
-        f.write(json.dumps(data, sort_keys = False))
-
-def applySettings(settings):
-    charlie.speaker.set_volume(settings['options']['Audio-Volume'] * 0.9, 'Beep')
-    charlie.speaker.set_volume(settings['options']['EFX-Volume'] * 0.9, 'PCM')
-
-settings = OrderedDict({'options': OrderedDict({'Debug Driving': 2, 'Audio-Volume': 80, 'EFX-Volume': 25, 'Console-Log': True, 'Show Warnings': True, 'Show Errors': True}),
-            'values': {
-                'min': {'Debug Driving': 0, 'Audio-Volume': 0, 'EFX-Volume': 0, 'Console-Log': False, 'Show Warnings': False, 'Show Errors': False},
-                'max': {'Debug Driving': 2, 'Audio-Volume': 100, 'EFX-Volume': 100, 'Console-Log': True, 'Show Warnings': True, 'Show Errors': True}},
-            'types': {'Debug Driving': 'int', 'Audio-Volume': 'int', 'EFX-Volume': 'int', 'Console-Log': 'bool', 'Show Warnings': 'bool', 'Show Errors': 'bool'}
-            })
-
-
-#storeSettings(settings) # temporary for storing changes in keys of the dictionary, created above, to the file
-
-charlie = EV3Brick()
-
-def mainLoop():
-    global menuState
-    menuState, oldMenuState, position = 0, 0, 0
-    oldPos = 1
-    loop, selected = True, False
-    tools.drawMenu(int(menuState))
-    keys = list(settings['options'].keys())
-    applySettings(settings)
-    while loop:
-        # navigation inbetween main pages
-        if menuState == 0:
-            if Button.UP in charlie.buttons.pressed():
-                menuState = 5
-            elif Button.DOWN in charlie.buttons.pressed():
-                menuState = 1
-        elif menuState > 0 and menuState < 6:
-            if Button.UP in charlie.buttons.pressed() and menuState > 1:
-                menuState -= 1
-            elif Button.UP in charlie.buttons.pressed() and menuState == 1:
-                menuState = 5
-            if Button.DOWN in charlie.buttons.pressed() and menuState < 5:
-                menuState += 1
-            elif Button.DOWN in charlie.buttons.pressed() and menuState == 5:
-                menuState = 1
-        
-        # subMenus
-        elif menuState == 50: #settings Menu
-
-            if Button.UP in charlie.buttons.pressed():
-                if not selected:
-                    if position > 0:
-                        position -= 1
-                    elif position == 0:
-                        position = len(settings['options']) - 1
-                else:
-                    if settings['options'][keys[position]] < settings['values']['max'][keys[position]]:
-                        settings['options'][keys[position]] += 1
-                    elif settings['options'][keys[position]] == settings['values']['max'][keys[position]]:
-                        settings['options'][keys[position]] = settings['values']['min'][keys[position]]
-                    tools.sound('assets/media/click.wav')
-                    tools.drawSettings(position, settings, selected)
-            if Button.DOWN in charlie.buttons.pressed():
-                if not selected:
-                    if position < len(settings['options']) - 1:
-                        position += 1
-                    elif position == len(settings['options']) - 1:
-                        position = 0
-                else:
-                    if settings['options'][keys[position]] > settings['values']['min'][keys[position]]:
-                        settings['options'][keys[position]] -= 1
-                    elif settings['options'][keys[position]] == settings['values']['min'][keys[position]]:
-                        settings['options'][keys[position]] = settings['values']['max'][keys[position]]
-                    tools.sound('assets/media/click.wav')
-                    tools.drawSettings(position, settings, selected)
-                        
-
-
-            if Button.CENTER in charlie.buttons.pressed():
-                if selected:
-                    storeSettings(settings)
-                    applySettings(settings)
-                    
-                selected = not selected
-                oldPos += 1
-
-            if position != oldPos:
-                tools.sound('assets/media/click.wav')
-                tools.drawSettings(position, settings, selected)
-                oldPos = position
-                time.sleep(0.07)
-
-        # selection of pages
-        if Button.RIGHT in charlie.buttons.pressed() and menuState > 0 and menuState <= 5 and not selected:
-            menuState = menuState * 10
-            position, oldPos = 0, 1
-            oldMenuState = menuState
-            tools.sound('assets/media/confirm.wav')
-            time.sleep(0.08)
-            tools.animate(menuState, True)
-            time.sleep(0.4)
-
-        if Button.LEFT in charlie.buttons.pressed() and menuState >= 10 and not selected:
-            tools.sound('assets/media/confirm.wav')
-            time.sleep(0.08)
-            tools.animate(menuState, False)
-            menuState = menuState / 10
-            oldMenuState = menuState
-            time.sleep(0.4)
-
-
-        if oldMenuState != menuState:
-            tools.sound('assets/media/click.wav')
-            time.sleep(0.08)
-            tools.drawMenu(menuState)
-            oldMenuState = menuState
-            time.sleep(0.3)
-
-        if self.logger.getScreenRefreshNeeded == 1:
-            self.logger.setScreenRefreshNeeded = 0
-            menuState = menuState / 10
-
-            
-
-# just some example code
-#charlie.sound.beep()
-#movements.execute([7, 75, 10, 0])
-
-
+      
 ### things, I'm sometimes using to test things - can be ignored
 """
 lineMap = {'height' : 300, 'width' : 1000,
@@ -164,7 +20,7 @@ lineMap = {'height' : 300, 'width' : 1000,
 
 tools.doIntersect(lineMap)"""
 
-settings = loadSettings()
+
 #runs menu
-mainLoop()
+ui.mainLoop()
 
---
+++ ui.py
@@ -0,0 +1,108 @@
+
+
+#storeSettings(settings) # temporary for storing changes in keys of the dictionary, created above, to the file
+
+charlie = EV3Brick()
+
+def mainLoop():
+    global menuState
+    menuState, oldMenuState, position = 0, 0, 0
+    oldPos = 1
+    loop, selected = True, False
+    tools.drawMenu(int(menuState))
+    keys = list(settings['options'].keys())
+    applySettings(settings)
+    while loop:
+        # navigation inbetween main pages
+        if menuState == 0:
+            if Button.UP in charlie.buttons.pressed():
+                menuState = 5
+            elif Button.DOWN in charlie.buttons.pressed():
+                menuState = 1
+        elif menuState > 0 and menuState < 6:
+            if Button.UP in charlie.buttons.pressed() and menuState > 1:
+                menuState -= 1
+            elif Button.UP in charlie.buttons.pressed() and menuState == 1:
+                menuState = 5
+            if Button.DOWN in charlie.buttons.pressed() and menuState < 5:
+                menuState += 1
+            elif Button.DOWN in charlie.buttons.pressed() and menuState == 5:
+                menuState = 1
+        
+        # subMenus
+        elif menuState == 50: #settings Menu
+
+            if Button.UP in charlie.buttons.pressed():
+                if not selected:
+                    if position > 0:
+                        position -= 1
+                    elif position == 0:
+                        position = len(settings['options']) - 1
+                else:
+                    if settings['options'][keys[position]] < settings['values']['max'][keys[position]]:
+                        settings['options'][keys[position]] += 1
+                    elif settings['options'][keys[position]] == settings['values']['max'][keys[position]]:
+                        settings['options'][keys[position]] = settings['values']['min'][keys[position]]
+                    tools.sound('assets/media/click.wav')
+                    tools.drawSettings(position, settings, selected)
+            if Button.DOWN in charlie.buttons.pressed():
+                if not selected:
+                    if position < len(settings['options']) - 1:
+                        position += 1
+                    elif position == len(settings['options']) - 1:
+                        position = 0
+                else:
+                    if settings['options'][keys[position]] > settings['values']['min'][keys[position]]:
+                        settings['options'][keys[position]] -= 1
+                    elif settings['options'][keys[position]] == settings['values']['min'][keys[position]]:
+                        settings['options'][keys[position]] = settings['values']['max'][keys[position]]
+                    tools.sound('assets/media/click.wav')
+                    tools.drawSettings(position, settings, selected)
+                        
+
+
+            if Button.CENTER in charlie.buttons.pressed():
+                if selected:
+                    storeSettings(settings)
+                    applySettings(settings)
+                    
+                selected = not selected
+                oldPos += 1
+
+            if position != oldPos:
+                tools.sound('assets/media/click.wav')
+                tools.drawSettings(position, settings, selected)
+                oldPos = position
+                time.sleep(0.07)
+
+        # selection of pages
+        if Button.RIGHT in charlie.buttons.pressed() and menuState > 0 and menuState <= 5 and not selected:
+            menuState = menuState * 10
+            position, oldPos = 0, 1
+            oldMenuState = menuState
+            tools.sound('assets/media/confirm.wav')
+            time.sleep(0.08)
+            tools.animate(menuState, True)
+            time.sleep(0.4)
+
+        if Button.LEFT in charlie.buttons.pressed() and menuState >= 10 and not selected:
+            tools.sound('assets/media/confirm.wav')
+            time.sleep(0.08)
+            tools.animate(menuState, False)
+            menuState = menuState / 10
+            oldMenuState = menuState
+            time.sleep(0.4)
+
+
+        if oldMenuState != menuState:
+            tools.sound('assets/media/click.wav')
+            time.sleep(0.08)
+            tools.drawMenu(menuState)
+            oldMenuState = menuState
+            time.sleep(0.3)
+
+        if self.logger.getScreenRefreshNeeded == 1:
+            self.logger.setScreenRefreshNeeded = 0
+            menuState = menuState / 10
+
+  
