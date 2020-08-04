e6ab481dd2c7f10e93dad35053b0b9a686329af5
XilefTech <XilefTech@users.noreply.github.com>
2020-08-05 01:27:45 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 
 import time, math, json
-import OSTools as tools
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
-from collections import OrderedDict
 from charlieosx import CharlieOSX
-import config, ui
+#import config, ui
 
       
 ### things, I'm sometimes using to test things - can be ignored
@@ -19,8 +17,8 @@ lineMap = {'height' : 300, 'width' : 1000,
             'obstacles' : [((2, 1), (4, 1))]}
 
 tools.doIntersect(lineMap)"""
-os = CharlieOSX('/config.yaml', '/settings.json', '/logs/')
+os = CharlieOSX('config.cfg', 'settings.json', '/logs/')
 
 #runs menu
-ui.mainLoop()
+#ui.mainLoop()
 
