2df334cee8bbe3b6abe18439a8d249a57a7a095f
XielfTech <52332196+XilefTech@users.noreply.github.com>
2020-09-03 00:55:09 +0200

    added docstrings to logger + removed config param

--- logging.py
+++ logging.py
@@ -2,11 +2,17 @@ import time, _thread, os
 from pybricks.parameters import Color, Button
 from pybricks.media.ev3dev import Image, ImageFile, Font, SoundFile
 
-#TODO Logging into Log file
 class Logger:
-    '''Logging method for CharlieOSX'''
-    def __init__ (self, config, settings, fileLocation, brick):
-        self.__config = config
+    '''
+    The Logger Class is used for Logging in both the log-files and into the console.
+    It also handles displaying warns and errors on the display (can be disabled).
+
+    Args:
+        settings (dict): The parsed settings that are used for setting or disabling the depth of logging
+        fileLocation (dict): The location (foldername) where the log files are stored
+        brick (EV3Brick): EV3Brick used for accessing the display
+    '''
+    def __init__ (self, settings, fileLocation, brick):
         self.__settings = settings
         if fileLocation == '':
             if not 'logs' in os.listdir():
@@ -39,12 +45,24 @@ class Logger:
         return 'Logger'
 
     def __sound(self, file):
+        '''
+        This private method is used for playing a sound in a separate thread so that other code can be executed simultaneously.
+
+        Args:
+            file (str / pybricks.media.ev3dev.SoundFile): The path to the soundfile to play
+        '''
         def __playSoundFile(soundFile):
             with self.__sound_lock:
                 self.__brick.speaker.play_file(soundFile)
         _thread.start_new_thread(__playSoundFile, (file, ))
 
     def getFormattedTime(self):
+        '''
+        Gets the current time (UTC) and formats it as [DD.MM.YYYY-hh:mm:ss]
+        
+        Returns:
+        str: the formatted time as a string
+        '''
         ts = time.localtime(time.time())
         timeString = '['
         format = [2, '.', 1, '.', 0, '-', 3, ':', 4, ':', 5]
@@ -60,22 +78,47 @@ class Logger:
         return timeString
 
     def debug(self, method, msg):
+        '''
+        This method formats the given inputs as <[time] [MethodName] [Debug] msg>.
+        The output of this fuction can be disabled by setting the "Logging-Level" in the settings to 1 or higher.
+
+        Args:
+            method (obj): A passthrough of the object calling this fuction
+            msg (str): The debug message
+        '''
         if self.__settings['options']['Logging-level'] <= 0:
             print(self.getFormattedTime(), '[%s] [Debug]' % str(method), msg)
             self.__logFile.write('%s [%s] [Debug] %s\n' % (self.getFormattedTime(), str(method), msg))
 
     def info(self, method, msg):
-        ''' Makes a log output, without showing anything on the EV3 screen'''
+        '''
+        This method formats the given inputs as <[time] [MethodName] [Info] msg>.
+        The output of this fuction can be disabled by setting the "Logging-Level" in the settings to 2 or higher.
+
+        Args:
+            method (obj): A passthrough of the object calling this fuction
+            msg (str): The info message
+        '''
         if self.__settings['options']['Logging-level'] <= 1:
             print(self.getFormattedTime(), '[%s] [Info]' % str(method), msg)
             self.__logFile.write('%s [%s] [Info] %s\n' % (self.getFormattedTime(), str(method), msg))
 
     def warn(self, method, msg):
+        '''
+        This method formats the given inputs as <[time] [MethodName] [Warn] msg>.
+        The output of this fuction can be disabled by setting the "Logging-Level" in the settings to 3 or higher.
+        Also dependent on wether or not "Show Warnings" is disabled or not in the settings,
+        this function will create a 'popup' on-screen with the given warn-message.
+
+        Args:
+            method (obj): A passthrough of the object calling this fuction
+            msg (str): The warn message
+        '''
         if self.__settings['options']['Logging-level'] <= 2:
             print(self.getFormattedTime(), '[%s] [Warning]' % str(method), msg)
             self.__logFile.write('%s [%s] [Warning] %s\n' % (self.getFormattedTime(), str(method), msg))
         
-        if self.__settings['options']['Show Errors']:
+        if self.__settings['options']['Show Warnings']:
             self.__sound(SoundFile.GENERAL_ALERT)
             self.__brick.screen.draw_image(26, 24, 'assets/graphics/notifications/warn.png', transparent = Color.RED)
             self.__brick.screen.draw_text(31, 34, msg, text_color = Color.BLACK)
@@ -102,6 +145,17 @@ class Logger:
             self.__refreshScreenNeeded = 1
 
     def error(self, method, msg, exception):
+        '''
+        This method formats the given inputs as <[time] [MethodName] [Wrror] msg>.
+        The output of this fuction can be disabled by setting the "Logging-Level" in the settings to 4 or higher.
+        Also dependent on wether or not "Show Errors" is disabled or not in the settings,
+        this function will create a 'popup' on-screen with the given warn-message.
+
+        Args:
+            method (obj): A passthrough of the object calling this fuction
+            msg (str): The error message
+            exception (exception / str): string or exception of the error that occured
+        '''
         if self.__settings['options']['Logging-level'] <= 3:
             print(self.getFormattedTime(), '[%s] [Error] %s: %s: %s' % (str(method), msg, type(exception).__name__, str(exception)))
             self.__logFile.write('%s [%s] [Error] %s: %s: %s\n' % (self.getFormattedTime(), str(method), msg, type(exception).__name__, str(exception)))
@@ -132,9 +186,19 @@ class Logger:
             self.__refreshScreenNeeded = 1
 
     def getScreenRefreshNeeded(self):
+        '''
+        function to get Variable value
+        
+        Returns:
+        bool: wether or not the screen needs to be "refreshed" or overridden
+        '''
         return self.__refreshScreenNeeded
     
     def setScreenRefreshNeeded(self, value):
+        '''
+        function to set Variable value
+        
+        Args:
+        bool: wether or not the screen should be refreshed
+        '''
         self.__refreshScreenNeeded = value
-
-    
