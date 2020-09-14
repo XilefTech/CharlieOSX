7752e24f79bcb968b10bcb6873ca98a51b529487
XielfTech <52332196+XilefTech@users.noreply.github.com>
2020-09-15 00:18:46 +0200

    merged menus and box into tools

--- UI/tools.py
+++ UI/tools.py
@@ -1,3 +1,23 @@
+from UI.UIObject import UIObject
+
+
+class Menu():
+    '''This Method defines a Menu for a given Page'''
+
+    def __init__(self, type):
+        self.type = type
+        self.objects = []
+
+    def addObject(self, object: UIObject):
+        self.objects.append(object)
+
+    def draw(self):
+        for i in self.objects:
+            i.draw()
+
+    def rasterize(self):
+        return 'WIP'
+
 class Box:
     def __init__(self, x, y, width, height):
         self.x = x
