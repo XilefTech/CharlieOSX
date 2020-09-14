251585c890f7e6224d0e36fa3edaa51b307532a1
XielfTech <52332196+XilefTech@users.noreply.github.com>
2020-09-15 00:15:38 +0200

    renamed to box.py

--- UI/rect.py
+++
@@ -1,9 +0,0 @@
-class Box:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def contains(self, other):
-        return ((self.x > other.x and self.x < (other.x+other.width)) and (self.y > other.y and self.y < (other.y+other.height)))
