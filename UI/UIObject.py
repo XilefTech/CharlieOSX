d56d8100d874ce5f298f9e2889c5c72707a1ae55
Gerhard Benkovsky <benkovskigeri@gmail.com>
2020-09-26 15:30:21 +0200

    Added own UI Implementation. Is still very buggy.

--- UI/UIObject.py
+++ UI/UIObject.py
@@ -2,19 +2,50 @@ from profileHelper import ProfileHelper
 from pybricks.parameters import Button, Color
 from pybricks.media.ev3dev import Image, ImageFile, Font, SoundFile
 
-#from UI.tools import Box
+# from UI.tools import Box
 
 
 class UIObject:
-    def __init__(self, name: str, brick, bounds: Box, contentType, contentPosition: tuple, content):
-        #self.logger = logger
+    def __init__(self, name: str, brick, bounds: Box, contentType, content):
+        # self.logger = logger
         self.name = name
         self.brick = brick
         self.bounds = bounds
         self.contentType = contentType
-        self.contentPosition = contentPosition
         self.content = content
+        self.radius = 0
+        self.selected = False
+
+        self.bounds.y *= self.bounds.height
+
+    def update(self):
+        if self.selected:
+            self.radius = 10
+        else:
+            self.radius = 0
 
     def draw(self):
         if self.contentType == 'img':
-            self.brick.screen.draw_image(self.contentPosition[0], self.contentPosition[1], self.content, transparent = Color.RED)
+            self.brick.screen.draw_image(
+                self.bounds.x + 5, self.bounds.y + 4, self.content, transparent=Color.RED)
+            self.brick.screen.draw_box(
+                self.bounds.x, self.bounds.y, self.bounds.width+1, self.bounds.height, r=self.radius, fill=False, color=Color.BLACK)
+            if self.selected:
+                self.drawInfoBox()
+
+    def drawInfoBox(self):
+
+        self.brick.screen.draw_box(
+            self.bounds.width + 2, 0, self.brick.screen.width-1, self.brick.screen.height-1, r=5, fill=False, color=Color.BLACK)
+
+        self.brick.screen.set_font(Font(size=14))
+
+        self.brick.screen.draw_text(
+            self.bounds.width + 5, 0, self.name, text_color=Color.BLACK, background_color=None)
+
+    def clear(self):
+        self.brick.screen.draw_box(
+            self.bounds.x, self.bounds.y, self.bounds.width+1, self.bounds.height, r=0, fill=True, color=Color.WHITE)
+
+        self.brick.screen.draw_box(
+            self.bounds.width, 0, self.brick.screen.width, self.brick.screen.height-1, r=0, fill=True, color=Color.WHITE)
