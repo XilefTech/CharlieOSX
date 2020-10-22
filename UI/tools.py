455438f20515f9e8b16377e1f7af5bcbe5bfb300
XielfTech <52332196+XilefTech@users.noreply.github.com>
2020-10-23 00:23:14 +0200

    added: rasterize: sort objects in subarrays by... x value

--- UI/tools.py
+++ UI/tools.py
@@ -24,6 +24,7 @@ class Menu():
     def rasterize(self):
         array = []
         todo = copy(self.objects)
+        # group the UIObjects in their y ranges
         while todo != []:
             x = todo.pop(0)
             array.append([])
@@ -32,7 +33,9 @@ class Menu():
                 if i.bounds.getMidHeight() in range(x.bounds.y, x.bounds.y + x.bounds.height + 1):
                     array[len(array) - 1].append(i)
                     todo.pop(0)
-        
+        # sort objects in the subarrays by x value
+        for i in array:
+            i.sort(key=lambda x: x.bounds.x, reverse=False)
         return array
 
 class Box():
