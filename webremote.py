ca4c23e01b97e63370841ad19c33cf484fd1ef85
XielfTech <52332196+XilefTech@users.noreply.github.com>
2020-10-26 01:12:30 +0100

    added getting of the sent data

--- webremote.py
+++ webremote.py
@@ -17,9 +17,27 @@ class Webremote():
         
             for line in htmlFile:
                 yield from resp.awrite(line)
+
+        @self.app.route("/api")
+        def api(req, resp):
+            yield from picoweb.start_response(resp, content_type = "text/html")
+            
+            proc = req.qs.split("&")
+            self.outDict = {}
+            for elm in proc:
+                temp = elm.split("=")
+                try:
+                    self.outDict[temp[0]] = int(temp[1])
+                except:
+                    self.outDict[temp[0]] = temp[1]
+
     def startServerThread(self):
         def runWebserver():
             with self.weblock:
                 self.app.run(debug = -1, host = '192.168.178.52')
         _thread.start_new_thread(runWebserver, ())
+
+    def getResponseData(self):
+        return self.outDict
+
 
