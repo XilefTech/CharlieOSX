bd828ca66ac38ccf0ce08b4bdb7aba957783f67a
TheGreyDiamond <s-einkauf@oest-rs.de>
2020-12-17 23:20:48 +0100

    Small handling improvments

--- logging.py
+++ logging.py
@@ -208,4 +208,7 @@ class Logger:
         Args:
         bool: wether or not the screen should be refreshed
         '''
-        self.__refreshScreenNeeded = value
+        if(isinstance(value, bool)):
+            self.__refreshScreenNeeded = value
+        else:
+            self.warn(self, "Tried calling setScreenRefreshNeeded with a non boolean argument, expected True or False, got: " + str(value))
