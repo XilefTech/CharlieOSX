86504d20afb301444e6dc0cd6c02b9e89eecb413
TheGreyDiamond <s-einkauf@oest-rs.de>
2020-12-17 23:17:25 +0100

    Small change

--- logging.py
+++ logging.py
@@ -37,6 +37,12 @@ class Logger:
         self.__refreshScreenNeeded = 0
         self.__sound_lock = _thread.allocate_lock()
         self.info(self, 'Logger initialized')  
+        try:
+            f = open("VERSION", "r")
+            ver = f.readlines()[0].replace("\n", "").replace("\r", "")
+            self.info(self, 'Using version ' + str(ver))  
+        except:
+            self.warn(self, 'Failed reading version from file')  
     
     def __repr__(self):
         return 'TODO'
