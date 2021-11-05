f93a39a39ec150e0ee8516aebc70c18c212110df
XilefTech <52332196+XilefTech@users.noreply.github.com>
2021-11-05 17:58:02 +0100

    worked on ScriptOne

--- scripts.py
+++ scripts.py
@@ -1,7 +1,8 @@
 
 from robot import Charlie
 from math import pi
-from pybricks.parameters import Stop
+from pybricks.parameters import (Stop, Color)
+import time
 
 class Scripts():
     '''runs'''
@@ -20,13 +21,41 @@ class Scripts():
         ]
 
     def runOne(self):
-        '''the first script (Wing, Packet, Bridge and ...)'''
-        self.robot.__rMotor.run_angle(250, self.cmToDeg(115), then=Stop.BRAKE, wait=False)
-        self.robot.__rMotor.run_angle(250, self.cmToDeg(115), then=Stop.BRAKE)
+        '''the first script (FLL-Run 2021/22: Wing, Packet, Bridge, helicoper and wagon cargo)'''
+        ## drive to doorstep
+        self.robot.__rMotor.run_angle(700, self.cmToDeg(123), then=Stop.BRAKE, wait=False)
+        self.robot.__lMotor.run_angle(700, self.cmToDeg(123), then=Stop.BRAKE, wait=True)
+        
+        ## manuver out of the corner
+        self.robot.straight(10, -3, 0)
+        self.robot.turn(30, 5, 23)
+        self.robot.breakMotors()
+        self.robot.straight(20, -22, 0)
+        self.robot.turn(10, -1, 23)
+        self.robot.breakMotors()
+        self.robot.straight(15, 10, 0)
 
-        self.robot.straight(20, -5, 0)
-        self.robot.turn(20, -45, 23)
-        self.robot.straight(50, 20, 0)
+        ## to the helicopter and wagon
+        self.robot.absTurn(2, -57, 3)
+        self.robot.breakMotors()
+        time.sleep(0.5)
+        self.robot.straight(40, 60, 0)
+
+        while self.robot.__rLight.color() != Color.WHITE:
+            pass
+
+        self.robot.breakMotors()
+        time.sleep(0.2)
+        self.robot.straight(10, -2, 0)
+        self.robot.breakMotors()
+        self.robot.absTurn(10, 15, 2)
+        self.robot.straight(10, 2, 0)
+        self.robot.breakMotors()
+        self.robot.absTurn(10, 0, 2)
+
+
+        self.robot.breakMotors()
+        time.sleep(1)
 
     def runTwo(self):
         '''the second script'''
@@ -38,7 +67,6 @@ class Scripts():
         self.robot.breakMotors()
         self.robot.__aMotor1.run_ang(300, 200, then=Stop.COAST, wait=True)
 
-
     def runThree(self):
         '''the third script'''
         pass
@@ -49,4 +77,4 @@ class Scripts():
 
     def cmToDeg(self, dist: float) -> float:
         '''converts a measurement from centimeters to degrees the moevement motors have to turn'''
-        return dist / (self.config['wheelDiameter'] * pi)
+        return dist / (self.config['wheelDiameter'] * pi) * 360
