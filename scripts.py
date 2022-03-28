7d30ca92329262f9357cb54b3638a9c36b251aa5
XilefTech <52332196+XilefTech@users.noreply.github.com>
2022-03-28 16:47:35 +0200

    this year's runThree

--- scripts.py
+++ scripts.py
@@ -1,8 +1,8 @@
-
 from robot import Charlie
 from driving.robot import Robot
 from math import pi
 from pybricks.parameters import (Stop, Color)
+from pybricks.hubs import EV3Brick
 import time
 
 class Scripts():
@@ -16,50 +16,58 @@ class Scripts():
         self.config = config
 
         self.scriptList = [
-            self.runOne,
             self.runTwo,
             self.runThree,
+            self.runOne,
             self.runFour
         ]
 
     def runOne(self):
-        '''the first script (FLL-Run 2021/22: Wing, Packet, Bridge, helicoper and wagon cargo)'''
+        '''the first script (FLL-Run 2021/22: Wing, Packet, Bridge and wagon cargo)'''
         driving = self.robot.driving
 
         ## drive to doorstep
-        self.robot.__rMotor.run_angle(500, self.cmToDeg(130), then=Stop.BRAKE, wait=False)
-        self.robot.__lMotor.run_angle(500, self.cmToDeg(130), then=Stop.BRAKE, wait=True)
-        #self.robot.action(20, 7/8, 1)
+        driving.setAccel(5)
+        driving.straight(45, 107, 0)
         driving.breakMotors()
-        self.robot.brick.speaker.beep()
+        #self.robot.rMotor.run_angle(500, self.cmToDeg(108), then=Stop.Coast, wait=False)
+        #self.robot.lMotor.run_angle(500, self.cmToDeg(108), then=Stop.Coast, wait=True)
+        self.robot.aMotor1.run_angle(800, 90, wait=False)
+        driving.straight(12, 20, 0)
+        driving.breakMotors(coast=True)
+        time.sleep(0.2)
+        driving.setAccel(self.config["acceleration"]-5)
+        EV3Brick().speaker.beep()
 
-        ## manuver out of the corner
-        driving.straight(10, -3, 0)
+        driving.straight(35, -8, 0)
+        print("turning")
+        driving.absTurn(10, -45, 3, min_speed=100)
         driving.breakMotors()
-        driving.turn(30, 5, 23)
+        driving.straight(80, 52, 0)
+        driving.absTurn(10, 0, 23, min_speed=100)
         driving.breakMotors()
-        driving.straight(20, -22, 0)
-        driving.turn(10, -1, 23)
         driving.breakMotors()
-        driving.straight(15, 10, 0)
-
-        ## to the helicopter and wagon
-        driving.absTurn(2, -57, 3)
-        driving.breakMotors()
-        time.sleep(0.5)
-        driving.straight(40, 60, 0)
-
-        while self.robot.rLight.color() != Color.WHITE:
-            pass
-
+        self.robot.aMotor2.run_angle(90, 160)
+        EV3Brick().speaker.beep()
+        EV3Brick().speaker.beep()
+        driving.straight(50, -8, 0)
+        self.robot.aMotor2.run_angle(120, -90, wait=False)
+        driving.absTurn(30, 130, 23, min_speed=100)
         driving.breakMotors()
         time.sleep(0.2)
-        driving.straight(10, -2, 0)
+        driving.straight(60, 20, 0)
+        driving.absTurn(30, 148, 23, min_speed=80)
         driving.breakMotors()
-        driving.absTurn(10, 15, 2)
-        driving.straight(10, 2, 0)
+        time.sleep(0.2)
+        driving.straight(70, 39, 0)
         driving.breakMotors()
-        driving.absTurn(10, 0, 2)
+        self.robot.aMotor1.run_angle(300, -90)
+        driving.straight(50, -20, 0)
+        driving.absTurn(20, 175, 23, min_speed=50)
+        driving.breakMotors()
+        time.sleep(0.2)
+        driving.straight(100, 100, 0)
+
 
 
         driving.breakMotors()
@@ -72,46 +80,51 @@ class Scripts():
         self.robot.gyro.reset_angle(-45)
         time.sleep(0.5)
 
+        
+
+        time.sleep(100)
         ## to the crane
-        driving.straight(45, 65, 0)
-        driving.absTurn(20, -5, 23)
+        driving.straight(90, 75, 0)
+        driving.absTurn(10, -15, 23, min_speed=40)
         driving.breakMotors()
         self.robot.brick.speaker.beep()
         time.sleep(0.5)
 
         # push crane over
-        driving.straight(40, 58, 0)
+        driving.straight(90, 50, 0)
+        driving.absTurn(10, 8, 23, min_speed=40)
+        driving.straight(10, 15, 0)
         driving.breakMotors()
         self.robot.brick.speaker.beep()
 
         ## manuver around crane
-        driving.straight(20, -10, 0)
-        driving.absTurn(40, 45, 23)
+        driving.straight(70, -15, 0)
+        driving.absTurn(10, 45, 23, min_speed=40)
         driving.breakMotors()
         time.sleep(0.3)
-        driving.straight(35, 10, 0)
-        driving.absTurn(50, 0, 23)
+        driving.straight(80, 15, 0)
+        driving.absTurn(10, 0, 23, min_speed=40)
         driving.breakMotors()
         time.sleep(0.3)
-        driving.straight(35, 16, 0)
+        driving.straight(35, 25, 0)
 
         ## backwards to wall
-        driving.absTurn(50, 110, 23)
+        driving.absTurn(10, 130, 23, min_speed=40)
         driving.breakMotors()
         time.sleep(0.3)
-        driving.straight(20, -20, 0)
-        driving.absTurn(20, 90, 23)
+        driving.straight(50, -20, 0)
+        driving.absTurn(20, 90, 23, min_speed=40)
         driving.breakMotors()
 
-        self.robot.aMotor2.run_angle(300, 0.9 * 360, then=Stop.COAST, wait=False) # extend side arm
+        self.robot.aMotor2.run_angle(400, -180, then=Stop.COAST, wait=False) # extend side arm
 
         ## go to work
-        self.robot.aMotor1.run_angle(800, 600 * 3, then=Stop.HOLD, wait=True) # put down fork
+        self.robot.aMotor1.run_angle(900, -90 * (20/12), then=Stop.HOLD, wait=True) # put down fork
         time.sleep(0.3)
-        driving.straight(30, 22, 0)
+        driving.straight(30, 32, 0)
         driving.breakMotors()
-        self.robot.aMotor2.run_angle(300, -0.8 * 360, then=Stop.COAST, wait=False) # flip rail-piece
-        self.robot.aMotor1.run_angle(700, -500 * 3, then=Stop.HOLD, wait=True) # lift up containers
+        self.robot.aMotor2.run_angle(300, 180, then=Stop.COAST, wait=False) # flip rail-piece
+        self.robot.aMotor1.run_angle(700, 100, then=Stop.HOLD, wait=True) # lift up containers
         time.sleep(0.3)
 
         driving.straight(40, -50, 0)
@@ -123,53 +136,51 @@ class Scripts():
         driving = self.robot.driving
 
         self.robot.gyro.reset_angle(-90)
+        self.robot.aMotor1.run_angle(50, -17, then=Stop.BRAKE)
 
         ## to the container station
-        driving.straight(20, 12, 0)
-        driving.absTurn(20, -45, 3)
+        driving.straight(80, 67, 0)
+        driving.absTurn(20, -59, 3, min_speed=70)
         driving.breakMotors()
-        driving.straight(40, 73, 0)
-        driving.breakMotors()
-        self.robot.brick.speaker.beep()
-
-        driving.absTurn(20, 0, 2)
-        driving.breakMotors()
-        driving.straight(20, 5, 0) # 29? 19?
-        self.legacyRobot.toColor(20, 0, 2) 
-        driving.breakMotors()
-        driving.absTurn(20, -90, 3)
-        driving.breakMotors()
-        driving.straight(20, 7, 0)
+        driving.straight(50, 32, 0)
         driving.breakMotors()
         self.robot.brick.speaker.beep()
-        time.sleep(0.3)
+        driving.straight(50, -4, 0)
+        driving.breakMotors()
+        self.robot.aMotor1.run_angle(200, -90)
 
-        ## to the train line
-        driving.straight(30, -20, 0)
-        driving.absTurn(30, -10, 23)
+        ## cargo plane
+        driving.straight(80, -17, 0)
+        driving.absTurn(30, -133, 2, min_speed=80)
         driving.breakMotors()
-        driving.straight(35, 15, 0)
-        driving.absTurn(40, 105, 23)
+        driving.straight(30, 4, 0)
         driving.breakMotors()
-        driving.straight(35, -35, 0)
-        driving.absTurn(20, 91, 23)
+        self.robot.aMotor1.run_angle(200, 110, then=Stop.HOLD, wait=False)
+        driving.straight(30, -14, 0)
         driving.breakMotors()
-        time.sleep(1)
-        driving.straight(50, 40, 0)
+        self.robot.aMotor1.run_angle(400, -60)
+        driving.turn(10, 30, 23, min_speed=70)
         driving.breakMotors()
-        self.robot.brick.speaker.beep()
-        time.sleep(0.5)
+        self.robot.aMotor1.run_angle(200, 60)
+        driving.absTurn(20, -135, 3, min_speed=60)
+        self.robot.aMotor1.run_angle(800, -100)
 
-        ## back to home
-        driving.straight(50, -20, 0)
+        ## go do the plane
+        driving.absTurn(10, -330, 23, min_speed=100)
         driving.breakMotors()
-        driving.absTurn(40, 180, 23)
+        driving.straight(50, 6.5, 0)
+        driving.turn(30, 25, 2, min_speed=50)
         driving.breakMotors()
-        driving.straight(65, 55, 0)
-        self.legacyRobot.curve(65, 40, -40)
-        self.legacyRobot.curve(65, 40, 30)
-        driving.straight(65, 40, 0)
+        self.robot.aMotor2.run_angle(2000, 180)
+        driving.straight(90, -20, 0)
         driving.breakMotors()
+
+        ## homee
+        driving.absTurn(40, -270, 2, min_speed=100)
+        driving.straight(100, 50, 0)
+        driving.breakMotors()
+
+        
 
     def runFour(self):
         '''the fourth script'''
